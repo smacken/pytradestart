28822c355dbe39f5f99ab51f496681884a2fe87a
Scott Mackenzie <scott.mac.web@gmail.com>
2019-11-17 12:11:02 +1300

    version ano

--- pytradestart/__init__.py
+++ pytradestart/__init__.py
@@ -5,4 +5,5 @@ path_to_append = os.path.join(os.path.dirname(__file__), os.pardir, "mockportfol
 print(path_to_append)
 sys.path.append(path_to_append)
 
-__version__ = '0.1.0.' + str(round(time.time()))
+now = str(round(time.time()))
+__version__ = f'0.1.0.{now}'
