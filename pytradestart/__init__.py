cdb693fa5d3ce3956c7171639e61af9e0d876aba
Scott Mackenzie <scott.mac.web@gmail.com>
2019-11-17 12:02:26 +1300

    version

--- pytradestart/__init__.py
+++ pytradestart/__init__.py
@@ -1,7 +1,8 @@
 import sys
 import os
+import time
 path_to_append = os.path.join(os.path.dirname(__file__), os.pardir, "mockportfolio")
 print(path_to_append)
 sys.path.append(path_to_append)
 
-__version__ = '0.1.0'
+__version__ = '0.1.0.' + str(round(time.time()))
